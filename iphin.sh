58d25fe9ae0f3bac0b6895926e2a8d85c0e466b3
ammohanrao <rao.lms@gmail.com>
2018-10-20 21:01:01 +0530

    Update iphin.sh

--- iphin.sh
+++ iphin.sh
@@ -15,9 +15,9 @@ echo "Completed FILE 1"
 NOW=$(date +"%m-%d-%Y")
 FILE="iphin1_$NOW.txt"
 FILE1="iphin1_results_$NOW.txt"
-gawk -F'\t' '{printf("%s\n",$0)}' $FILE | grep -vw "\[\]" >$FILE1
+gawk -F'\t' '{printf("%s\n",$3)}' $FILE | grep -vw "\[\]" >$FILE1
 
 NOW=$(date +"%m-%d-%Y")
 FILE="iphin2_$NOW.txt"
 FILE1="iphin2_results_$NOW.txt"
-gawk -F'\t' '{printf("%s\n",$0)}' $FILE | grep -vw "\[\]" >$FILE1
+gawk -F'\t' '{printf("%s\n",$3)}' $FILE | grep -vw "\[\]" >$FILE1
