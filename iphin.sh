cff4195959fd6b346202d8c30308a713acd1ec88
MADAN MOHAN RAO <rao.lms@gmail.com>
2018-10-23 00:22:36 +0530

    Update iphin.sh

--- iphin.sh
+++ iphin.sh
@@ -1,4 +1,5 @@
-#!/bin/sh
+#!/bin/bash
+#!/usr/bin/env python
 
 NOW=$(date +"%m-%d-%Y")
 FILE="iphin1_$NOW.txt"
@@ -10,14 +11,25 @@ NOW=$(date +"%m-%d-%Y")
 FILE="iphin2_$NOW.txt"
 python3 iphin2.py >$FILE
 
-echo "Completed FILE 1"
+echo "Completed FILE 2"
+
+NOW=$(date +"%m-%d-%Y")
+FILE="iphin3_$NOW.txt"
+python3 iphin3.py >$FILE
+
+echo "Completed FILE 3"
 
 NOW=$(date +"%m-%d-%Y")
 FILE="iphin1_$NOW.txt"
 FILE1="iphin1_results_$NOW.txt"
-gawk -F'\t' '{printf("%s\n",$3)}' $FILE | grep -vw "\[\]" >$FILE1
+gawk -F'\t' '{printf("%s\n",$0)}' $FILE | grep -vw "\[\]" >$FILE1
 
 NOW=$(date +"%m-%d-%Y")
 FILE="iphin2_$NOW.txt"
 FILE1="iphin2_results_$NOW.txt"
-gawk -F'\t' '{printf("%s\n",$3)}' $FILE | grep -vw "\[\]" >$FILE1
+gawk -F'\t' '{printf("%s\n",$0)}' $FILE | grep -vw "\[\]" >$FILE1
+
+NOW=$(date +"%m-%d-%Y")
+FILE="iphin3_$NOW.txt"
+FILE1="iphin3_results_$NOW.txt"
+gawk -F'\t' '{printf("%s\n",$0)}' $FILE | grep -vw "\[\]" >$FILE1
